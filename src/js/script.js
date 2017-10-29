6bf1202cbc224c199e598324d37d3804b48d51ff
Filip M <filipm93@gmail.com>
2017-10-29 15:47:40 +0100

    chart.html

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,35 @@
 $(function(){
-	
+
+	// new training select field styling
 	$('#new-training-select').select2({
 		theme: "classic",
 		width: 'style'
 	});
 
+	// unfold and fold excercise
 	$('.excercise').children('.inner').children('.name').click(function(){
-		console.log('huj');
 		$(this).parent().parent().toggleClass('unfolded');
+	});
+
+	// chart
+	let ctx = document.getElementById('chart').getContext('2d');
+	let chart = new Chart(ctx, {
+		// The type of chart we want to create
+		type: 'line',
+
+		// The data for our dataset
+		data: {
+			labels: ["January", "February", "March", "April", "May", "June", "July"],
+			datasets: [{
+				label: "My First dataset",
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgb(255, 99, 132)',
+				data: [0, 10, 5, 2, 20, 30, 45],
+			}]
+		},
+
+		// Configuration options go here
+		options: {}
 	});
 
 });
